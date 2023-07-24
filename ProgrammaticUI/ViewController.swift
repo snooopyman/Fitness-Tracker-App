f89335775e7c2461bded6b91d637f1b5c0d1be01
Armando Caceres <armando.caceres@nomasystems.com>
2023-07-24 20:46:41 +0200

    Initial Commit

---
+++ ProgrammaticUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgrammaticUI
+//
+//  Created by Jesús Armando Cáceres Vilchez on 24/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
